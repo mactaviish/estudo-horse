1680832b4481754706e8b3ab64008ffdbd6b7a5b
Leonardo Dalla Corte <leonardo.brancalione@estudante.uffs.edu.br>
2022-07-08 11:58:33 -0300

    alteracoes

--- backend/backend.dpr
+++ backend/backend.dpr
@@ -18,10 +18,10 @@ begin
 
   Users := TJSONArray.Create;
 
-  App.Get('/ping',
+  App.Get('/users',
     procedure(_Req: THorseRequest; _Res: THorseResponse; _Next: TProc)
     begin
-      _Res.Send<TJSONArray>(Users);
+      _Res.Send<TJSONAncestor>(Users.Clone);
     end);
 
   App.Post('/users',
@@ -34,5 +34,15 @@ begin
       _Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
     end);
 
-    App.Listen;
+  App.Delete('/users/:id',
+    procedure(_Req: THorseRequest; _Res: THorseResponse; _Next: TProc)
+    var
+      Id: Integer;
+    begin
+      Id := _Req.Params.Items['id'].ToInteger;
+      Users.Remove(Pred(Id)).Free;
+      _Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
+    end);
+
+  App.Listen;
 end.
