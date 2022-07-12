8b68cb60c9dbfd2709a678eb29454a69a2bcbba9
Leonardo Dalla Corte <leonardo.brancalione@estudante.uffs.edu.br>
2022-07-12 07:52:05 -0300

    aulas

--- backend/backend.dpr
+++ backend/backend.dpr
@@ -5,7 +5,8 @@ program backend;
 {$R *.res}
 
 uses
-  System.SysUtils, System.JSON, Horse, Horse.Commons, Horse.Jhonson, Horse.BasicAuthentication;
+  System.SysUtils, System.JSON, Horse, Horse.Commons, Horse.Jhonson,
+  Horse.BasicAuthentication, Horse.Compression, Horse.HandleException;
 
 var
   App: THorse;
@@ -15,7 +16,9 @@ begin
   App := THorse.Create;
   App.Port := 9000;
 
+  App.Use(Compression());
   App.Use(Jhonson);
+  App.Use(HandleException);
   App.Use(HorseBasicAuthentication(
     function(const AUsername, APassword: string): Boolean
     begin
@@ -24,14 +27,37 @@ begin
 
   Users := TJSONArray.Create;
 
+  App.Get('/exception',
+    procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TProc)
+    var
+      LConteudo: TJSONObject;
+    begin
+      raise EHorseException.Create('Error Message');
+      LConteudo := TJSONObject.Create;
+      LConteudo.AddPair('nome', 'Leonardo');
+      ARes.Send(LConteudo);
+    end);
+
+  App.Get('/ping',
+    procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TProc)
+    var
+      I: Integer;
+      LPong: TJSONArray;
+    begin
+      LPong := TJSONArray.Create;
+      for I := 0 to 1000 do
+        LPong.Add(TJSONObject.Create(TJSONPair.Create('ping', 'pong')));
+      ARes.Send(LPong);
+    end);
+
   App.Get('/users',
-    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TProc)
     begin
       ARes.Send<TJSONAncestor>(Users.Clone);
     end);
 
   App.Post('/users',
-    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TProc)
     var
       User: TJSONObject;
     begin
@@ -41,7 +67,7 @@ begin
     end);
 
   App.Delete('/users/:id',
-    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; ANext: TProc)
     var
       Id: Integer;
     begin
