9d1a9009fd373968849fe1d11d1f8b9b686ccfea
Leonardo Dalla Corte <leonardo.brancalione@estudante.uffs.edu.br>
2022-07-12 07:12:30 -0300

    alterações

--- backend/backend.dpr
+++ backend/backend.dpr
@@ -5,7 +5,7 @@ program backend;
 {$R *.res}
 
 uses
-  System.SysUtils, Horse, Horse.Commons, Horse.Jhonson, System.JSON;
+  System.SysUtils, System.JSON, Horse, Horse.Commons, Horse.Jhonson, Horse.BasicAuthentication;
 
 var
   App: THorse;
@@ -13,35 +13,41 @@ var
 
 begin
   App := THorse.Create;
-  App.Use(Jhonson);
   App.Port := 9000;
+
+  App.Use(Jhonson);
+  App.Use(HorseBasicAuthentication(
+    function(const AUsername, APassword: string): Boolean
+    begin
+      Result := AUsername.Equals('user') and APassword.Equals('password');
+    end));
 
   Users := TJSONArray.Create;
 
   App.Get('/users',
-    procedure(_Req: THorseRequest; _Res: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
     begin
-      _Res.Send<TJSONAncestor>(Users.Clone);
+      ARes.Send<TJSONAncestor>(Users.Clone);
     end);
 
   App.Post('/users',
-    procedure(_Req: THorseRequest; _Res: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
     var
       User: TJSONObject;
     begin
-      User := _Req.Body<TJSONObject>.Clone as TJSONObject;
+      User := AReq.Body<TJSONObject>.Clone as TJSONObject;
       Users.AddElement(User);
-      _Res.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
+      ARes.Send<TJSONAncestor>(User.Clone).Status(THTTPStatus.Created);
     end);
 
   App.Delete('/users/:id',
-    procedure(_Req: THorseRequest; _Res: THorseResponse; _Next: TProc)
+    procedure(AReq: THorseRequest; ARes: THorseResponse; _Next: TProc)
     var
       Id: Integer;
     begin
-      Id := _Req.Params.Items['id'].ToInteger;
+      Id := AReq.Params.Items['id'].ToInteger;
       Users.Remove(Pred(Id)).Free;
-      _Res.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
+      ARes.Send<TJSONAncestor>(Users.Clone).Status(THTTPStatus.NoContent);
     end);
 
   App.Listen;
